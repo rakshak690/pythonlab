4899336aeec6ce9e79572a0a5eb5d8db9405dfaf
rakshak690 <127379524+rakshak690@users.noreply.github.com>
2023-07-23 23:01:10 +0530

    Update prgm2 jobseq.py

--- DAA/prgm2 jobseq.py
+++ DAA/prgm2 jobseq.py
@@ -25,3 +25,23 @@ if __name__=='__main__':
         jobs.append(Job(taskId,deadline,profit))
     T=int(input("Enter the total time: "))
     scheduleJobs(jobs,T)
+    /*OUTPUT
+    Enter the no of jobs: 5
+Enter the taskId for job 1:1
+Enter the deadline for job 1:2
+Enter the profit for job 1:20
+Enter the taskId for job 2:2
+Enter the deadline for job 2:2
+Enter the profit for job 2:15
+Enter the taskId for job 3:3
+Enter the deadline for job 3:1
+Enter the profit for job 3:10
+Enter the taskId for job 4:4
+Enter the deadline for job 4:3
+Enter the profit for job 4:5
+Enter the taskId for job 5:5
+Enter the deadline for job 5:3
+Enter the profit for job 5:1
+Enter the total time: 3
+The scheduled jobs are:  ['2', '1', '4']
+Total profit:  40 */
